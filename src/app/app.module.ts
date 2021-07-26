50bac655961678275b3907cfe4e102a9fef834a5
Alin Florin <alin@qoffice.ro>
2021-07-26 19:32:19 +0300

    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,24 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MsalBroadcastService,
+  MsalGuard,
+  MsalInterceptor,
+  MsalModule,
+  MsalService,
+  ProtectedResourceScopes,
+} from '@azure/msal-angular';
+import {
+  BrowserCacheLocation,
+  InteractionType,
+  PublicClientApplication,
+} from '@azure/msal-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+const isIE =
+  window.navigator.userAgent.indexOf('MSIE ') > -1 ||
+  window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
   declarations: [
@@ -16,15 +34,51 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     NotFoundComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FlexLayoutModule
+    HttpClientModule,
+    FlexLayoutModule,
+    MsalModule.forRoot(
+      new PublicClientApplication({
+        auth: {
+          clientId: 'ce50bd0d-4018-4d43-98e1-bcb373e994ab', // This is your client ID
+          authority: '7a567291-3704-4d96-8e5f-9c93f6e4bc2b', // This is your tenant ID
+          redirectUri: 'http://localhost:4200/', // This is your redirect URI
+        },
+        cache: {
+          cacheLocation: BrowserCacheLocation.LocalStorage,
+          storeAuthStateInCookie: isIE, // Set to true for Internet Explorer 11
+        },
+      }),
+      {
+        interactionType: InteractionType.Redirect,
+        authRequest: {
+          scopes: ['user.read'],
+        },
+      },
+      {
+        interactionType: InteractionType.Redirect,
+        protectedResourceMap: new Map<
+          string,
+          (string | ProtectedResourceScopes)[] | null
+        >(),
+      }
+    ),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
