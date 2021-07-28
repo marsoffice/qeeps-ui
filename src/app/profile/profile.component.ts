b20aca50c4d06f354dc5c46e77f1e40c2602e2cd
Alin Florin <alin@qoffice.ro>
2021-07-28 11:53:34 +0300

    f

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AccountInfo } from '@azure/msal-browser';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -6,11 +8,20 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
-
-  constructor(public authService: AuthService) { }
+export class ProfileComponent implements OnInit, OnDestroy {
+  private _destroy: Subscription[] = [];
+  user: AccountInfo | null = null;
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this._destroy.push(
+      this.authService.user.subscribe(u => {
+        this.user = u;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
 
   }
 
