91cebf9bec3a2760ba027eb398ee1c22be341c0a
Alin Florin <alin@qoffice.ro>
2021-07-30 15:31:02 +0300

    -

--- src/app/services/easy-auth.interceptor.ts
+++ src/app/services/easy-auth.interceptor.ts
@@ -18,11 +18,47 @@ export class EasyAuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return this.authService.user.pipe(
       switchMap(user => {
-        const jwt = request.headers.get('authorization');
-        if (user != null && jwt != null) {
+        if (user != null) {
+          console.log(user);
+          const userClaims = user.idTokenClaims as any;
+          const claims = {
+            auth_typ: 'aad',
+            claims: [
+              {
+                typ: 'name',
+                val: userClaims.name
+              },
+              {
+                typ: 'http://schemas.microsoft.com/identity/claims/emailaddress',
+                val: user.username
+              },
+              {
+                typ: 'http://schemas.microsoft.com/identity/claims/objectidentifier',
+                val: user.localAccountId
+              }
+            ]
+          };
+          if (userClaims.roles != null) {
+            for (let role of userClaims.roles) {
+              claims.claims.push({
+                typ: 'roles',
+                val: role
+              });
+            }
+          }
+          if (userClaims.groups != null) {
+            for (let group of userClaims.groups) {
+              claims.claims.push({
+                typ: 'groups',
+                val: group
+              });
+            }
+          }
+
+          const b64Principal = btoa(JSON.stringify(claims));
           let headers = request.headers;
           headers = headers.append( 'X-MS-CLIENT-PRINCIPAL-IDP', 'aad');
-          headers = headers.append( 'X-MS-CLIENT-PRINCIPAL', (jwt as string).substring(6));
+          headers = headers.append( 'X-MS-CLIENT-PRINCIPAL', b64Principal);
           headers = headers.append( 'X-MS-CLIENT-PRINCIPAL-ID', user.localAccountId);
           headers = headers.append( 'X-MS-CLIENT-PRINCIPAL-NAME', user.name as string);
           const newRequest = request.clone({headers: headers});
