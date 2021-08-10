6b0f32adc7aadd26633ac144480deac31debe07f
Alin Florin <alin@qoffice.ro>
2021-08-10 16:17:50 +0300

    d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit, OnDestroy {
   private _destroy: Subscription[] = [];
   user: AccountInfo | null = null;
 
-  constructor( private mediaObserver: MediaObserver,translate: TranslateService) {
+  constructor(private msalService: MsalService, private mediaObserver: MediaObserver, private authService: AuthService, translate: TranslateService) {
     translate.setDefaultLang('ro');
     const localStorageLanguage = localStorage.getItem('lang');
     translate.use(localStorageLanguage == null ? translate.getBrowserLang() : localStorageLanguage);
@@ -26,10 +26,21 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.isIframe = window !== window.parent && !window.opener;
-
+    this.authService.updateLoggedInStatus();
+    this._destroy.push(
+      this.mediaObserver.asObservable().subscribe(() => {
+        this.isMobile = this.mediaObserver.isActive('xs');
+      })
+    );
+    this._destroy.push(
+      this.authService.user.subscribe(u => {
+        this.user = u;
+      })
+      );
   }
 
   ngOnDestroy(): void {
+    this.authService.destroy();
     this._destroy.forEach(x => x.unsubscribe());
   }
 }
