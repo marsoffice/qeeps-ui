34cf793230275b2c6c8d458f41d0618149e549a7
Alin Florin <alin@qoffice.ro>
2021-08-16 16:54:07 +0300

    f

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   adtenantid: (window as any)?.envConfig?.adtenantid,
   adclientid: (window as any)?.envConfig?.adclientid,
-  languages: ['ro', 'en']
+  languages: ['ro', 'en'],
+  publicvapidkey: 'BJyVzZqrjdEBC6JhUbhslT3HTAP8oAe1TCj4Vt-xcuDA67GRYcvYFivzbpyes3DkD5X9itEjL-tZXGSWCg3X6DI'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@ export const environment = {
   production: false,
   adtenantid: localenv.adtenantid,
   adclientid: localenv.adclientid,
-  languages: ['ro', 'en']
+  languages: ['ro', 'en'],
+  publicvapidkey: 'BJyVzZqrjdEBC6JhUbhslT3HTAP8oAe1TCj4Vt-xcuDA67GRYcvYFivzbpyes3DkD5X9itEjL-tZXGSWCg3X6DI'
 };
 
 import 'zone.js/plugins/zone-error';
