c62134c11076a0bbd5cb0064eec921e0079c3b26
Alin Florin <alin@qoffice.ro>
2021-08-18 22:44:13 +0300

    f

---
+++ lws.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  port: 4200,
+  key: 'ssl/cert.key',
+  cert: 'ssl/cert.crt',
+  https: true,
+  rewrite: [
+    {
+      from: '/api/access/(.*)',
+      to: 'http://localhost:7001/api/access/$1'
+    },
+    {
+      from: '/api/forms/(.*)',
+      to: 'http://localhost:7002/api/forms/$1'
+    },
+    {
+      from: '/api/notifications/(.*)',
+      to: 'http://localhost:7003/api/notifications/$1'
+    }
+  ],
+  directory: 'dist/qeeps-ui',
+  logFormat: 'stats'
+}
