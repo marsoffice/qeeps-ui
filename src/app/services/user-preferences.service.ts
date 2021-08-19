41de798ab7c0b145e29e7ffd0235dbf4d1196e1a
Alin Florin <alin@qoffice.ro>
2021-08-19 13:30:49 +0300

    f

--- src/app/services/user-preferences.service.ts
+++ src/app/services/user-preferences.service.ts
@@ -22,6 +22,9 @@ export class UserPreferencesService {
       if (x == null) {
         return;
       }
+      if (upLs == null) {
+        localStorage.setItem('user_preferences', JSON.stringify(x));
+      }
       const old = this.userPreferencesSubject.value;
       if (x?.preferredLanguage !== old?.preferredLanguage || x?.useDarkTheme !== old?.useDarkTheme) {
         this.userPreferencesSubject.next(x);
