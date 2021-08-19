a92caff1c042f0e1e8aadf52be2ffa9943d3dbcd
Alin Florin <alin@qoffice.ro>
2021-08-19 19:46:10 +0300

    r

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -160,7 +160,10 @@ if (!environment.production) {
       }
     ),
       ServiceWorkerModule.register('ngsw-worker.js', {
-        enabled: environment.production
+        enabled: environment.production,
+        // Register the ServiceWorker as soon as the app is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000'
       }),
   ],
   providers: [
