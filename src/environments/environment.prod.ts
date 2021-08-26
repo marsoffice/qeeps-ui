6017a929ce33e9ad94369b58699e36a53f4e39d0
Alin Florin <alin@qoffice.ro>
2021-08-27 01:12:21 +0300

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   adtenantid: (window as any)?.envConfig?.adtenantid,
   adclientid: (window as any)?.envConfig?.adclientid,
+  usefunctionproxy: (window as any)?.envConfig?.usefunctionproxy,
   languages: ['ro', 'en'],
   publicvapidkey: (window as any)?.envConfig?.publicvapidkey,
   notificationsElementsPerPage: 50,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   production: false,
   adtenantid: localenv.adtenantid,
   adclientid: localenv.adclientid,
+  usefunctionproxy: 'false',
   languages: ['ro', 'en'],
   publicvapidkey: localenv.publicvapidkey,
   notificationsElementsPerPage: 50,
