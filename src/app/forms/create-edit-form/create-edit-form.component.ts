4e990c14231e31312d41440a3e4bb2afffb0681e
Alin Florin <alin@qoffice.ro>
2021-08-31 22:44:18 +0300

    f

--- src/app/forms/create-edit-form/create-edit-form.component.ts
+++ src/app/forms/create-edit-form/create-edit-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, Validators, FormControl} from '@angular/forms';
+import {FormGroup, Validators, FormControl, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-create-edit-form',
@@ -7,17 +7,38 @@ import {FormGroup, Validators, FormControl} from '@angular/forms';
   styleUrls: ['./create-edit-form.component.scss']
 })
 export class CreateEditFormComponent implements OnInit {
-  form = new FormGroup({
-    title: new FormControl(null, [Validators.required, Validators.minLength(6)]),
-    description: new FormControl(),
-    attachments: new FormControl([]),
-    isLocked: new FormControl(false),
-    lockedUntilDate: new FormControl(),
-    rowAppendDisabled: new FormControl(false),
-    isRecurrent: new FormControl(false),
+  form: FormGroup;
+  attachments: FormArray;
+  columns: FormArray;
+  rows: FormArray;
+  formAccesses: FormArray;
+  constructor() {
+    this.columns = new FormArray([]);
 
-  });
-  constructor() { }
+    this.attachments = new FormArray([]);
+
+    this.rows = new FormArray([]);
+
+    this.formAccesses = new FormArray([]);
+
+    this.form = new FormGroup({
+      title: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      description: new FormControl(),
+      attachments: this.attachments,
+      isLocked: new FormControl(false),
+      lockedUntilDate: new FormControl(),
+      rowAppendDisabled: new FormControl(false),
+      isRecurrent: new FormControl(false),
+      cronExpression: new FormControl(),
+      isPinned: new FormControl(false),
+      pinnedUntilDate: new FormControl(),
+      tags: new FormControl([]),
+      columns: this.columns,
+      rows: this.rows,
+      formAccesses: this.formAccesses
+    });
+
+  }
 
   ngOnInit(): void {
   }
