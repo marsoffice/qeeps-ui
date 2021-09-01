f29ca62e8591ff7ff362bc5dec3d015ac9722049
Alin Florin <alin@qoffice.ro>
2021-09-01 11:22:13 +0300

    h

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -13,7 +13,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { FileUploadModule } from '@iplab/ngx-file-upload';
 
 
 @NgModule({
@@ -32,7 +31,6 @@ import { FileUploadModule } from '@iplab/ngx-file-upload';
     MatIconModule,
     MatButtonModule,
     MatSelectModule,
-    FileUploadModule,
   ],
   providers: [
     FormsService
