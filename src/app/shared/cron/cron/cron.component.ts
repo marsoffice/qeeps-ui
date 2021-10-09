a2f157ffe542a49f5de084dc586b0544c2489dd3
Alin Florin <alin@qoffice.ro>
2021-10-09 13:56:00 +0300

    s

--- src/app/shared/cron/cron/cron.component.ts
+++ src/app/shared/cron/cron/cron.component.ts
@@ -25,7 +25,17 @@ export class CronComponent implements OnInit, ControlValueAccessor {
   onTouched = () => { };
 
   selectedRecurrenceType = RecurrenceType.Year;
+  selectedMinute = 0;
+  allMinutes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59];
 
+  selectedHour = 0;
+  allHours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+
+  selectedDay = 1;
+  allDays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  allMonths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  selectedMonth = 1;
 
   constructor() { }
 
@@ -62,5 +72,16 @@ export class CronComponent implements OnInit, ControlValueAccessor {
       this.onTouched();
       this.touched = true;
     }
+  }
+
+  padValue(x: number): string {
+    if (x == null) {
+      return x;
+    }
+    let str = x.toString();
+    if (str.length === 1) {
+      return `0${str}`;
+    }
+    return str;
   }
 }
