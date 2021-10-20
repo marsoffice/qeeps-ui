b2266e0952e9d6378cd0bbccbd98e08f8a205363
Alin Florin <alin@qoffice.ro>
2021-10-20 22:59:22 +0300

    f

--- src/app/shared/cron/cron/cron.component.ts
+++ src/app/shared/cron/cron/cron.component.ts
@@ -26,22 +26,22 @@ export class CronComponent implements OnInit, ControlValueAccessor {
   isInitialized = false;
 
   selectedRecurrenceType = RecurrenceType.Year;
-  selectedMinute: number | null = 0;
+  selectedMinute: number | null = null;
   allMinutes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59];
 
-  selectedHour: number | null = 0;
+  selectedHour: number | null = null;
   allHours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
-  selectedDay: number | null = 1;
+  selectedDay: number | null = null;
   allDays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
   allMonths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  selectedMonth: number | null = 1;
+  selectedMonth: number | null = null;
 
   selectedYear: number | null = null;
 
   selectedWeekday: string | null = null;
-  selectedSecond: number | null = 0;
+  selectedSecond: number | null = null;
   allWeekdays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
 
   constructor() { }
@@ -54,6 +54,38 @@ export class CronComponent implements OnInit, ControlValueAccessor {
       }));
     if (this.disabledRecurrenceTypes != null) {
       this.recurrenceTypes = this.recurrenceTypes!.filter(x => this.disabledRecurrenceTypes!.indexOf(x.value) === -1);
+      if (this.disabledRecurrenceTypes.indexOf(RecurrenceType.Second) > -1) {
+        this.selectedSecond = 0;
+      }
+      if (this.disabledRecurrenceTypes.indexOf(RecurrenceType.Minute) > -1) {
+        this.selectedSecond = 0;
+        this.selectedMinute = 0;
+      }
+      if (this.disabledRecurrenceTypes.indexOf(RecurrenceType.Hour) > -1) {
+        this.selectedSecond = 0;
+        this.selectedMinute = 0;
+        this.selectedHour = 0;
+      }
+      if (this.disabledRecurrenceTypes.indexOf(RecurrenceType.Day) > -1) {
+        this.selectedSecond = 0;
+        this.selectedMinute = 0;
+        this.selectedHour = 0;
+        this.selectedDay = 1;
+      }
+      if (this.disabledRecurrenceTypes.indexOf(RecurrenceType.Week) > -1) {
+        this.selectedSecond = 0;
+        this.selectedMinute = 0;
+        this.selectedHour = 0;
+        this.selectedDay = 1;
+        this.selectedWeekday = null;
+      }
+      if (this.disabledRecurrenceTypes.indexOf(RecurrenceType.Month) > -1) {
+        this.selectedSecond = 0;
+        this.selectedMinute = 0;
+        this.selectedHour = 0;
+        this.selectedDay = 1;
+        this.selectedMonth = 1;
+      }
     }
   }
 
