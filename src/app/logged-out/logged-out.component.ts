1b25d57ee4aa29080217b72e11c7bdf223f4c214
Alin Florin <alin@qoffice.ro>
2021-10-26 11:39:10 +0300

    g

--- src/app/logged-out/logged-out.component.ts
+++ src/app/logged-out/logged-out.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-logged-out',
   templateUrl: './logged-out.component.html',
   styleUrls: ['./logged-out.component.scss']
 })
-export class LoggedOutComponent implements OnInit {
-
-  constructor() { }
+export class LoggedOutComponent implements OnInit, OnDestroy {
+  private _destroy: Subscription[] = [];
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this._destroy.push(
+      this.authService.user.subscribe(u => {
+        if (u != null) {
+          this.router.navigate(['']);
+        }
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this._destroy.forEach(x => x.unsubscribe());
   }
 
 }
