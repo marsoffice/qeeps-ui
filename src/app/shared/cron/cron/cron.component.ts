a8f70f7a1ae76cd396778d4bf43684e2daa64803
Alin Florin <alin@qoffice.ro>
2021-11-27 12:35:41 +0200

    s

--- src/app/shared/cron/cron/cron.component.ts
+++ src/app/shared/cron/cron/cron.component.ts
@@ -32,11 +32,11 @@ export class CronComponent implements OnInit, ControlValueAccessor {
   selectedHour: number | null = 0;
   allHours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
-  selectedDay: number | null = 1;
+  selectedDay: number | null = null;
   allDays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
   allMonths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  selectedMonth: number | null = 1;
+  selectedMonth: number | null = null;
 
   selectedYear: number | null = null;
 
