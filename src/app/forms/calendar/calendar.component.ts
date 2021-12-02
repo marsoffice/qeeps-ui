b763a911a9049b3797a969fb3e9e8070e33c0654
Alin Florin <alin@qoffice.ro>
2021-12-02 15:51:06 +0200

    -

--- src/app/forms/calendar/calendar.component.ts
+++ src/app/forms/calendar/calendar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { DateAdapter } from '@angular/material/core';
 import { MatCalendar } from '@angular/material/datepicker';
+import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
 import { FormDto } from '../models/form.dto';
 import { FormsService } from '../services/forms.service';
@@ -14,12 +16,20 @@ export class CalendarComponent implements OnInit, OnDestroy {
   private _destroy: Subscription[] = [];
 
   @ViewChild('calendar', { static: true }) calendar!: MatCalendar<any>;
-  constructor(private formsService: FormsService) { }
+  constructor(private formsService: FormsService,
+    private translateService: TranslateService,
+    private dateAdapter: DateAdapter<any>) { }
 
   ngOnInit(): void {
     this._destroy.push(
       this.calendar.stateChanges.subscribe(() => {
         console.log(this.calendar);
+      })
+    );
+
+    this._destroy.push(
+      this.translateService.onLangChange.subscribe(e => {
+        this.dateAdapter.setLocale(e.lang);
       })
     );
   }
